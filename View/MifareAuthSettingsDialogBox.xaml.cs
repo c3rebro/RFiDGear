c5f87787e7b66c699398e47a7b5ef086566b00ef
Steven <c3rebro[at]gmail.com>
2017-01-08 00:30:08 +0100

    renamed authdialog

---
+++ View/MifareAuthSettingsDialogBox.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+
+namespace RFiDGear.View
+{
+	/// <summary>
+	/// Interaction logic for MifareAuthSettingsDialogBox.xaml
+	/// </summary>
+	public partial class MifareAuthSettingsDialogBox
+	{
+		public MifareAuthSettingsDialogBox()
+		{
+			InitializeComponent();
+		}
+		
+		private void OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+		{
+			e.Column.Header = ((PropertyDescriptor)e.PropertyDescriptor).DisplayName;
+		}
+	}
+}
