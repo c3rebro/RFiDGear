9aa492d86eeb3dfb1772400cb6233a1a45f790fc
C3rebro <mail@shansen-online.de>
2021-03-16 20:18:19 +0100

    add "Encryption Type" to rfiddevice
    + add: show encryption settings when available

--- RFiDGear/DataAccessLayer/RFiDDevice.cs
+++ RFiDGear/DataAccessLayer/RFiDDevice.cs
@@ -52,6 +52,8 @@ namespace RFiDGear
 
 		public byte MaxNumberOfAppKeys { get; private set; }
 
+		public byte EncryptionType { get; private set; }
+
 		public uint FreeMemory { get; private set; }
 
 		public FileSetting DesfireFileSetting { get; private set; }
@@ -963,7 +965,33 @@ namespace RFiDGear
                                                 cmd.CreateLinearRecordFile((byte)_fileNo, _encMode, accessRights, (uint)_fileSize, (uint)_maxNbOfRecords);
                                                 break;
                                         }
+
+										return ERROR.NoError;
                                     }
+
+									switch (_fileType)
+									{
+										case FileType_MifareDesfireFileType.StdDataFile:
+											cmd.CreateStdDataFile((byte)_fileNo, _encMode, accessRights, (uint)_fileSize);
+											break;
+
+										case FileType_MifareDesfireFileType.BackupFile:
+											cmd.CreateBackupFile((byte)_fileNo, _encMode, accessRights, (uint)_fileSize);
+											break;
+
+										case FileType_MifareDesfireFileType.ValueFile:
+											cmd.CreateValueFile((byte)_fileNo, _encMode, accessRights, (uint)_minValue, (uint)_maxValue, (uint)_initValue, _isValueLimited);
+											break;
+
+										case FileType_MifareDesfireFileType.CyclicRecordFile:
+											cmd.CreateCyclicRecordFile((byte)_fileNo, _encMode, accessRights, (uint)_fileSize, (uint)_maxNbOfRecords);
+											break;
+
+										case FileType_MifareDesfireFileType.LinearRecordFile:
+											cmd.CreateLinearRecordFile((byte)_fileNo, _encMode, accessRights, (uint)_fileSize, (uint)_maxNbOfRecords);
+											break;
+									}
+
 
 									return ERROR.NoError;
 								}
@@ -1239,7 +1267,7 @@ namespace RFiDGear
 
 							if (card.Type == "DESFire" ||
 							    card.Type == "DESFireEV1" ||
-							    card.Type == "DESFireEV2")
+							    card.Type == "DESFireEV2" || card.Type == "GENERIC_T_CL_A")
 							{
 								var cmd = card.Commands as IDESFireCommands;
 
@@ -1255,7 +1283,8 @@ namespace RFiDGear
 										try
 										{
 											cmd.GetKeySettings(out keySettings, out maxNbrOfKeys);
-											MaxNumberOfAppKeys = maxNbrOfKeys;
+											MaxNumberOfAppKeys = (byte)(maxNbrOfKeys & 0x0F);
+											EncryptionType = (byte)(maxNbrOfKeys & 0xF0);
 											DesfireAppKeySetting = keySettings;
 
 											return ERROR.NoError;
@@ -1266,7 +1295,8 @@ namespace RFiDGear
 										}
 									}
 									cmd.GetKeySettings(out keySettings, out maxNbrOfKeys);
-									MaxNumberOfAppKeys = maxNbrOfKeys;
+									MaxNumberOfAppKeys = (byte)(maxNbrOfKeys & 0x0F);
+									EncryptionType = (byte)(maxNbrOfKeys & 0xF0);
 									DesfireAppKeySetting = keySettings;
 
 									return ERROR.NoError;
@@ -1472,15 +1502,23 @@ namespace RFiDGear
                                                 return ERROR.NoError;
                                             }
 
-                                            catch (Exception e)
+											catch
                                             {
-                                                if (e.Message != "" && e.Message.Contains("status does not allow the requested command"))
-                                                {
-                                                    return ERROR.NotAllowed;
-                                                }
-                                                else
-                                                    return ERROR.AuthenticationError;
-                                            }
+												try
+												{
+													cmd.Authenticate((byte)_keyNumberCurrent, aiToUse.MasterApplicationKey);
+													cmd.ChangeKey((byte)_keyNumberTarget, applicationMasterKeyTarget);
+												}
+												catch (Exception e)
+												{
+													if (e.Message != "" && e.Message.Contains("status does not allow the requested command"))
+													{
+														return ERROR.NotAllowed;
+													}
+													else
+														return ERROR.AuthenticationError;
+												}
+											}
                                         }
 									}
 
